2b939b98e2437233eecac50f8f64b7b13e0c7181
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2020-03-23 21:53:09 +0000

    Add createrar command
    Added the command that generates the message with the roles ready to be reacted to

--- src/channels/welcome/WelcomeHandler.ts
+++ src/channels/welcome/WelcomeHandler.ts
@@ -1,5 +1,6 @@
-import { Client, Message, PartialMessage, MessageReaction } from "discord.js";
+import { Client, Message, PartialMessage, MessageReaction, TextChannel, Emoji } from "discord.js";
 import IChannel from "../IChannel";
+import { checkMessageIsACommand } from "../../utils";
 
 export default class WelcomeHandler implements IChannel {
     /**
@@ -24,8 +25,43 @@ export default class WelcomeHandler implements IChannel {
      */
     private setupMessageEvents(): void {
         this.CLIENT.on("message", ((msg: Message | PartialMessage) => {
-
+            this.createReactaroleMessage(msg);
         }));
+    }
+
+    // just wanna note, I'm aware this only works in the welcome channel now. But it saves us
+    // effort having to track the message ID's in the db lol or the app itself as say a config var
+    // tTODO: create a full command line based solution
+    private createReactaroleMessage(message: Message | PartialMessage) {
+        if ((message.channel as TextChannel).name === "test" && message.content.substring(0, 11) === "!createrar") {
+            const fe = this.CLIENT.emojis.cache.get("691650017936670750").toString();
+            const be = this.CLIENT.emojis.cache.get("691650381842743326").toString();
+            const de = this.CLIENT.emojis.cache.get("691650936040325130").toString();
+            const ui = this.CLIENT.emojis.cache.get("691650966503686165").toString();
+            message.channel.send(`**Please enter your area(s) of expertise**:
+
+................................................
+
+        ${fe} - Front-End
+
+        ${be} - Back-End
+
+        ${de} - DevOps
+
+        ${ui} - UI/UX
+
+................................................
+            `).then(async msg => {
+                // better way to do this...?
+                await msg.react("691650017936670750");
+                await msg.react("691650381842743326");
+                await msg.react("691650936040325130");
+                await msg.react("691650966503686165");
+            });
+
+        }
+
+
     }
 
     /**
@@ -33,6 +69,7 @@ export default class WelcomeHandler implements IChannel {
      */
     private setupMessageReactionAddEvents(): void {
         this.CLIENT.on("messageReactionAdd", (messageReaction: MessageReaction) => {
+            console.log(messageReaction.emoji);
         });
     }
 
